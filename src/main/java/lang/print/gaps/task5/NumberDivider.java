a6aca122d92da3a8076272aeea2445fb2535d473
litim smail <litimsmail@gmail.com>
2024-08-14 12:35:07 +0100

    Task 5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+       int result = toBeDivided /5;
+        System.out.println(result);
+    }
+    public static void main (String [] args){
+        NumberDivider Dev = new NumberDivider();
+        Dev.divide(35);
+
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        float secondes = minutes * 60;
+        System.out.println(secondes);
+    }
+    public  static void main (String [] args){
+        TimeConvertor T = new TimeConvertor();
+        T.convert(10);
     }
 }
